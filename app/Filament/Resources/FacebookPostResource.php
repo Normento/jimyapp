d3cb1f5481642851fe66f3d7af267200cda9f7a2
Normento <normento229@gmail.com>
2024-10-22 17:27:18 +0100

    fix beug

--- app/Filament/Resources/FacebookPostResource.php
+++ app/Filament/Resources/FacebookPostResource.php
@@ -60,8 +60,8 @@ class FacebookPostResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('id')->sortable(),
-                Tables\Columns\TextColumn::make('rewrittenArticle.title')->label('Article')->sortable()->wrap()->toggleable()->searchable(),
                 Tables\Columns\ImageColumn::make('rewrittenArticle.image_url')->label('Image')->sortable()->wrap()->toggleable()->size(150)->searchable(),
+                Tables\Columns\TextColumn::make('rewrittenArticle.title')->label('Article')->sortable()->wrap()->toggleable()->searchable(),
                 Tables\Columns\TextColumn::make('facebookPage.name')->label('Page Facebook')->sortable()->searchable(),
                 Tables\Columns\TextColumn::make('facebook_post_id')->sortable()->wrap()->toggleable()->searchable(),
                 Tables\Columns\TextColumn::make('status')->sortable()->badge()->colors([
